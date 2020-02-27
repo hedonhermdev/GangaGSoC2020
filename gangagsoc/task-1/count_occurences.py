e4afc26a855d23f1840cc3b10f4d1a83488f798b
Tirth Jain <jaintirth24@gmail.com>
2020-02-28 03:57:06 +0530

    Add test for count_occurences.py

--- gangagsoc/task-1/count_occurences.py
+++ gangagsoc/task-1/count_occurences.py
@@ -9,7 +9,8 @@ def extract_text(pdf_file_name):
     text = text_bytes.decode("utf-8")
     return text
 
-def find_occurences(word, text):
+def num_occurences(word, text):
+    text = text.lower()
     matches = re.findall(rf"\b{word}\b", text)
     return len(matches)
 
@@ -18,6 +19,5 @@ if __name__ == '__main__':
     filename = sys.argv[1]
     word = 'the'
     text = extract_text(filename)
-    text = text.lower()
     num_occurences = find_occurences(word, text)
     print(num_occurences)
