6ee7e0f0be3906b14a43947ae71d6d517c19ca2c
Tirth Jain <jaintirth24@gmail.com>
2020-02-29 06:28:47 +0530

    Renamed directory task-1 to initial

--- gangagsoc/initial/ganga_job_splitter.py
+++ gangagsoc/initial/ganga_job_splitter.py
@@ -8,21 +8,21 @@ from ganga import Job, Executable, ArgSplitter, GangaDataset, LocalFile, CustomM
 pdf_files = os.listdir('./cern-pages/')
 
 # Create a new job
-j = Job(name='Count Word Occurences ')
+split_job = Job(name='Count Word Occurences ')
 
 # Set the job application as python3.
-j.application = Executable()
-j.application.exe = 'python3'
+split_job.application = Executable()
+split_job.application.exe = 'python3'
 
 # Give the python script and the PDF files as input to the job.
-j.inputfiles = [LocalFile('count_occurences.py'), LocalFile('cern-pages/*.pdf')]
+split_job.inputfiles = [LocalFile('count_occurences.py'), LocalFile('cern-pages/*.pdf')]
 
 # Split the jobs into subjobs, one for each PDF file.
-j.splitter = ArgSplitter()
-j.splitter.args = [['count_occurences.py', filename] for i, filename in enumerate(pdf_files, start=1)]
+split_job.splitter = ArgSplitter()
+split_job.splitter.args = [['count_occurences.py', filename] for i, filename in enumerate(pdf_files, start=1)]
 
 # Submit the job.
-j.submit()
+split_job.submit()
 
 # Start monitoring loop with 2 steps to wait for the job to completed.
 runMonitoring(steps=2, jobs=jobs[-1:])
@@ -31,6 +31,6 @@ runMonitoring(steps=2, jobs=jobs[-1:])
 merger = CustomMerger(files=['stdout'], module='./sum_merger.py', overwrite=True)
 
 # If the job has submitted, merge the outputs.
-if j.status == 'completed':
-    merger.merge(j, outputdir='./')
+if split_job.status == 'completed':
+    merger.merge(split_job, outputdir='./')
 
