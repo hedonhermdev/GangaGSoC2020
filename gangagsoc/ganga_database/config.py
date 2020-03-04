0b4aaf7013b0810e03fbea9c4a8083c8d4b74d52
Tirth Jain <jaintirth24@gmail.com>
2020-03-04 20:46:06 +0530

    Switch to a docker image of the MySQL server.

--- gangagsoc/ganga_database/config.py
+++ gangagsoc/ganga_database/config.py
@@ -2,7 +2,13 @@
 # Change these to match the values used in your database.
 class DBConfig:
     HOST = "localhost"
-    PORT = "3306"
+    PORT = "3308"
     USER = "gangauser"
     PASSWORD = "gangagsoc2020"
     DBNAME = "gangadb"
+
+class CacheConfig:
+    HOST = ""
+    PORT = ""
+    PASSWORD = ""
+
